ba49f454155479fb0d4c9a974241dad58279439c
Morten Teinum <morten.teinum@gmail.com>
2014-09-21 22:19:34 +0200

    Tatt bort litt kommentarer som heroku likte dårlig

--- src/main/java/no/teinum/flekkerol/TemperatureSensorCreator.java
+++ src/main/java/no/teinum/flekkerol/TemperatureSensorCreator.java
@@ -6,22 +6,6 @@ import org.eclipse.jetty.websocket.servlet.ServletUpgradeRequest;
 import org.eclipse.jetty.websocket.servlet.ServletUpgradeResponse;
 import org.eclipse.jetty.websocket.servlet.WebSocketCreator;
 
-// Vi har klienter som kjører i nettleser
-// Vi har sensorer som kjører på Raspberry Pi
-//
-// 1. Når en sensor kobles opp, skal vi registrere denne med sensorid
-// 2. En klient skal kunne få en liste med tilkoblet sensorer
-// 3. En klient skal kunne abonnere på data fra sensoren
-// 4. Dersom vi mister forbindelsen med en sensor må klienten ha beskjed
-// 5. Dersom klienten kobles fra, vil sensoren ta den bort fra sin liste
-//    grunnet io feil.
-// 6. Når en sensor kobles til skal alle klienter få beskjed
-//    På denne måten kan vi gjøre en "resubscribe" dersom sensor kobles av/på
-//
-//
-//
-
-
 public class TemperatureSensorCreator implements WebSocketCreator {
 
 	private static final Logger LOGGER = Logger
