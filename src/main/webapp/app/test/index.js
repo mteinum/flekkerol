51e3c1d380c6d0d2fa9b54364e88396b80bf6986
Morten Teinum <morten.teinum@gmail.com>
2014-09-24 22:05:46 +0200

    startet på termometer

--- src/main/webapp/app/test/index.js
+++ src/main/webapp/app/test/index.js
@@ -1,8 +1,91 @@
 define(['websocket', 'knockout', 'toastr', 'durandal/app'],
 		function(websocket, ko, toastr, app){
 	
+	
+	ko.bindingHandlers.canvas = {
+		    init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+		    	var ctx = element.getContext('2d');
+		    	valueAccessor()(ctx, element.width, element.height);
+		    }
+		};
+	
+	
 	return function(){
 		var self = this;
+		
+		self.currentTemperature = 0;
+		
+		self.drawTemperature = function(ctx, width, height){
+	        var targetTemperature = 63; // grader
+
+	        //  write markers
+	        var h_top = 20;
+	        var h_bottom = height - 30;
+
+	        var h_degree = (h_bottom - h_top) / 100;
+	        var y_zero = h_top + 100 * h_degree;
+
+	        function writeMarkers() {
+	        	for (var y = 0; y < 100; y += 10){
+	        		ctx.moveTo(10, h_top + y * h_degree);
+	        		ctx.lineTo(20, h_top + y * h_degree);
+	        	}
+
+	        	ctx.strokeStyle = '#fff';
+	        	ctx.stroke();
+	        }
+
+	        function writeTarget() {
+
+	        	var y_target = y_zero - (targetTemperature * h_degree);
+
+	        	ctx.beginPath();
+	        	ctx.moveTo(5, y_target);
+	        	ctx.lineTo(100, y_target);
+
+	        	ctx.strokeStyle = '#ff0000';
+	        	ctx.stroke();
+	        };
+
+	        function writeMeter(){
+
+	        	ctx.beginPath();
+
+	        	var y_temperature = y_zero - (self.currentTemperature * h_degree);
+
+	        	ctx.fillStyle = '#aaa';
+	        	ctx.fillRect(30, y_temperature, 20, y_zero - y_temperature);
+
+	        	ctx.strokeStyle = '#aaa';
+	        	ctx.font="20px Helvetica";
+
+	        	var txt = self.currentTemperature + '\xB0C';
+
+	        	ctx.fillText(txt, 60, y_temperature + 20);
+	        }
+
+	        function writeZeroLine(){
+	        	ctx.beginPath();
+	        	ctx.moveTo(10, y_zero);
+	        	ctx.lineTo(100, y_zero);
+	        	ctx.strokeStyle = '#aaa';
+	        	ctx.stroke();
+	        }
+
+	        function render(){
+	        	ctx.fillStyle = '#000';
+	        	ctx.fillRect(0, 0, width, height);
+	        	writeMarkers();
+	        	writeMeter();
+	        	writeZeroLine();
+	        	writeTarget();
+	        }
+
+	        (function animloop(){
+	          window.requestAnimationFrame(animloop);
+	          render();
+	        })();
+		};
 				
 		self.text = ko.observable();
 		
@@ -37,9 +120,14 @@ define(['websocket', 'knockout', 'toastr', 'durandal/app'],
 			});
 		});
 		
+		self.sensorDataSubscription = app.on('sensor-data').then(function(data){
+			self.currentTemperature = data.value / 1000;
+		});
+		
 		self.deactivate = function(){
 			self.sensorAddSubscription.off();
 			self.sensorListSubscription.off();
+			self.sensorDataSubscription.off();
 		};
 		
 		self.message = ko.observable();
